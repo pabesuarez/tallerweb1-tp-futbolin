81b4c3c8c4c7851f05cc8520a4079b72988c329e
mquiroz25 <mquiroz2535@gmail.com>
2018-06-24 12:45:48 -0300

    revert

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Direccion {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private String provincia;
 	private String localidad;
 	private String calle;
@@ -29,10 +29,10 @@ public class Direccion {
 		this.numero = numero;
 	}
 	
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getProvincia() {
